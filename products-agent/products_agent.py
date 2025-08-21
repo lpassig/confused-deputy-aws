1c8ab111222c09631fd15a94b363c0f6b59f12de
Ravi Panchal <ravi.panchal@hashicorp.com>
2025-08-21 17:03:36 +0800

    Remove secret references and clean up token handling

--- products-agent/products_agent.py
+++ products-agent/products_agent.py
@@ -46,7 +46,7 @@ When updating or deleting products, use search_products tool first to get the pr
 
 
 # Default token for testing - this will be replaced by the on-behalf-of token
-DEFAULT_JWT_TOKEN = None  # No default token
+DEFAULT_JWT_TOKEN = None  # No default token - must be provided
 
 
 class ProductsAgent:
